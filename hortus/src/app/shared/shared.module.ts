46800e3bb63c51b0a97cc5ba92de4d0042df98bf
be-molina <be.molina.landa@gmail.com>
2024-12-04 17:55:31 -0300

    components shared creados

--- hortus/src/app/shared/shared.module.ts
+++ hortus/src/app/shared/shared.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginInputComponent } from './components/login-input/login-input.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    LoginInputComponent,
+    LogoComponent
+  ],
+  exports: [
+    HeaderComponent,
+    LoginInputComponent,
+    LogoComponent,
+    FormsModule,
+    ReactiveFormsModule,
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule
   ]
 })
 export class SharedModule { }
