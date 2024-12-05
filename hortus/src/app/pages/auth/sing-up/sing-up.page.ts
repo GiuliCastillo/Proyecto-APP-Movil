65bdc3828b6b2e81c71a140e30879a391d430033
be-molina <be.molina.landa@gmail.com>
2024-12-05 00:38:34 -0300

    validaciones login, forgot password y signup

--- hortus/src/app/pages/auth/sing-up/sing-up.page.ts
+++ hortus/src/app/pages/auth/sing-up/sing-up.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingUpPage implements OnInit {
 
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit(){
+    if(this.form.valid){
+      console.log(this.form.value)
+    }
+  }
 }
