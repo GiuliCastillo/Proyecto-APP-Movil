5ef17fab108a5b575fdd076aa4068a23dbf6eb0c
__miu.ky <giu.castillo@duocuc.cl>
2024-12-05 01:56:39 -0300

    configuracion firebase

--- hortus/src/app/app.module.ts
+++ hortus/src/app/app.module.ts
@@ -7,9 +7,35 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { initializeApp } from 'firebase/app';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1diP1N0ViVt5GB2YoVs-ZJGt93ndPQ7k",
+  authDomain: "hortus-1685a.firebaseapp.com",
+  databaseURL: "https://hortus-1685a-default-rtdb.firebaseio.com",
+  projectId: "hortus-1685a",
+  storageBucket: "hortus-1685a.firebasestorage.app",
+  messagingSenderId: "1006176566816",
+  appId: "1:1006176566816:web:48757fe6abc2f51fee0645",
+  measurementId: "G-B50MTEPM33"
+};
+
+initializeApp(firebaseConfig);
+
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
