2f1cdd1e222385b2334fbb41fe076b7903e25e58
be-molina <be.molina.landa@gmail.com>
2024-12-11 16:57:52 -0300

    interfaz modal

--- hortus/src/app/shared/components/update-plants/update-plants.component.ts
+++ hortus/src/app/shared/components/update-plants/update-plants.component.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Plantas } from 'src/app/models/plantas.model';
+
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-update-plants',
   templateUrl: './update-plants.component.html',
   styleUrls: ['./update-plants.component.scss'],
 })
-export class UpdatePlantsComponent  implements OnInit {
+export class UpdatePlantsComponent implements OnInit {
 
-  constructor() { }
+  firebaseService = inject(FirebaseService);
+  utilsService = inject(UtilsService);
 
-  ngOnInit() {}
+  form = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('', [Validators.required]),
+    tipo: new FormControl('', [Validators.required]),
+    tiempoMaduracion: new FormControl('', [Validators.required]),
+    //img: new FormControl('', [Validators.required])
+
+
+  })
+
+
+
+  ngOnInit() {
+  }
+
+  async submit() {
+    //if (this.form.valid) {
+      //const loading = await this.utilsService.loading();
+
+      //await loading.present();
+      console.log(this.form.value);
+
+
+
+
+
+    //}
+  }
 
 }
+
+
+
